ad7a0fe1ab95915893f951f7767fd9677f996244
Daniel Barrett <dbarrett@seek.com.au>
2021-12-02 16:31:10 +1100

    2

--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,44 @@ module Main where
 import Text.Read (readMaybe)
 import Text.Show (Show)
 
-data Report = Increased | Decreased deriving (Show, Eq)
+data Report = Increased | Decreased | NoChange deriving (Show, Eq)
 
 main :: IO ()
-main = readInput1
+main = readInput2
 
+
+example :: Maybe [Int]
+example = toList "199 200 208 210 200 207 240 269 260 263"
+
+readInputFile :: IO (Maybe [Int])
+readInputFile = do
+    x <- readFile "input1.txt"
+    return $ toList x
 
 readInput1 = do
-    x <- readFile "input1.txt"
-    print $ countIncreased <$> measure <$> toList x
+    x <- readInputFile
+    print $ countIncreased <$> measure <$> x
+
+readInput2 = do
+    x <- readInputFile
+    print $ countIncreased <$> measure <$> rollingWindow <$> x
 
 toList :: String -> Maybe [Int]
 toList s = mapM readMaybe ( words s)
 
 measure :: [Int] -> [Report]
-measure (x:x':xs) = if x >= x' then Decreased : next else Increased : next
-    where next = measure (x':xs)
+measure (x:x':xs) = comparison x x' : measure (x':xs)
+    where comparison x y 
+            | x == y = NoChange
+            | x < y = Increased
+            | x > y = Decreased
+            | otherwise = NoChange
 measure [x] = []
 measure [] = []
 
 countIncreased :: [Report] -> Int
 countIncreased = foldl (\b a -> if a == Increased then b + 1 else b) 0
+
+rollingWindow :: [Int] -> [Int]
+rollingWindow (x:x':x'':xs) = x + x' + x'' : rollingWindow (x':x'':xs)
+rollingWindow _ = []
