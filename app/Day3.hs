29617c67300818d605a01fc69a7c76a7c951f44e
Daniel Barrett <barrett.t.daniel@gmail.com>
2021-12-07 20:38:41 +1100

    oxygenBitCriteria working

--- app/Day3.hs
+++ app/Day3.hs
@@ -1,4 +1,5 @@
 module Day3 (day3part1, day3part2) where
+
 import Data.List
 import Debug.Trace
 
@@ -6,11 +7,14 @@ getFile = readFile "input3.txt"
 
 example :: IO String
 example = do return "00100 11110 10110 10111 10101 01111 00111 11100 10000 11001 00010 01010"
-day3part1 = do  x <- getFile
-                putStrLn $ (("Day 3 Part 1: "++) . show . multiplyTuple . applyToTuple binaryToInt .  applyFunction notBinary . map mostCommon . transpose . parseInput) x
 
-day3part2 = do  x <- example
-                putStrLn $ (("Day 3 Part 2: "++) . show .oxygenGeneratorRating . parseInput) x
+day3part1 = do
+  x <- getFile
+  putStrLn $ (("Day 3 Part 1: " ++) . show . multiplyTuple . applyToTuple binaryToInt . applyFunction notBinary . map mostCommon . transpose . parseInput) x
+
+day3part2 = do
+  x <- example
+  putStrLn $ (("Day 3 Part 2: " ++) . show . oxygenGeneratorRating . parseInput) x
 
 parseInput :: String -> [[Int]]
 parseInput i = map (map readChar) $ words i
@@ -18,35 +22,36 @@ parseInput i = map (map readChar) $ words i
 readChar :: Char -> Int
 readChar = read . pure
 
-oxygenGeneratorRating :: [[Int]] -> [Int]
-oxygenGeneratorRating = oxygenBitCriteria 0
+oxygenGeneratorRating :: [[Int]] -> Int
+oxygenGeneratorRating ls = binaryToInt $ oxygenBitCriteria 0 ls
 
 oxygenBitCriteria :: Int -> [[Int]] -> [Int]
 oxygenBitCriteria _ [v] = v
 oxygenBitCriteria n vs
-    | null vs || n > length (head vs) - 1 = error $ show vs ++ " error!! " ++ show n
-    | length vs > 1 = trace (show n ++ " " ++ show vs ++ " " ++ show (filter (\xs -> (xs !! n) == common') vs) ++ " " ++ show common') oxygenBitCriteria (n+1) $ filter (\xs -> (xs !! n) == common') vs
-    where common' = common n vs
+--   | null vs || n > length (head vs) - 1 = error $ show vs ++ " error!! " ++ show n
+  | length vs > 1 = trace (show n ++ " " ++ show vs) oxygenBitCriteria (n + 1) $ filter (\xs -> trace ("filter: " ++ show xs ++ " n: " ++ show n) ((xs !! n) == common')) vs
+  where
+    common' = common n vs
 oxygenBitCriteria _ _ = []
 
 common :: Int -> [[Int]] -> Int
-common n =  mostCommon . map (!! n)
+common n vs  = trace ("common: n: " ++ show n ++ ", vs: "++ show vs) $ mostCommon $ map (!! n) vs
 
 mostCommon :: [Int] -> Int
-mostCommon l = case  partition (==1) l of
-    (x, y) -> if length x >= length y then x !! 1 else y !! 1
+mostCommon l = case partition (== 1) l of
+  (x, y) -> if length x >= length y then head x else head y
 
 binaryToInt :: [Int] -> Int
-binaryToInt xs = sum ( zipWith (\ x y -> x * 2 ^ y) (reverse xs) (iterate (+1) 0))
+binaryToInt xs = sum (zipWith (\x y -> x * 2 ^ y) (reverse xs) (iterate (+ 1) 0))
 
 notBinary :: [Int] -> [Int]
 notBinary = map (\x -> if x == 0 then 1 else 0)
 
-applyFunction :: (a -> b) -> a  -> (a, b)
+applyFunction :: (a -> b) -> a -> (a, b)
 applyFunction f a = (a, f a)
 
-applyToTuple :: (a -> b) -> (a,a) ->  (b, b)
-applyToTuple f (a,b) = (f a, f b)
+applyToTuple :: (a -> b) -> (a, a) -> (b, b)
+applyToTuple f (a, b) = (f a, f b)
 
 multiplyTuple :: Num a => (a, a) -> a
-multiplyTuple (a,b) = a * b
+multiplyTuple (a, b) = a * b
