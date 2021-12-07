2fe8dad0563a7144c7be44a16c823042aa8fc4ab
Daniel Barrett <barrett.t.daniel@gmail.com>
2021-12-07 20:47:25 +1100

    finish day 3

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,10 @@ import Day3 (day3part1, day3part2)
 
 main :: IO ()
 main = sequence_ [
-    -- day1part1, 
-    -- day1part2, 
-    -- day2part1, 
-    -- day2part2, 
-    -- day3part1, 
+    day1part1, 
+    day1part2, 
+    day2part1, 
+    day2part2, 
+    day3part1, 
     day3part2
     ]
