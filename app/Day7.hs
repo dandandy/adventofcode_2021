3e6944b41772c648f96da17710b0c2d0d679b74d
Daniel Barrett <barrett.t.daniel@gmail.com>
2021-12-08 20:53:59 +1100

    day 7 advent part 2

--- app/Day7.hs
+++ app/Day7.hs
@@ -12,7 +12,11 @@ day7part1 =  do x <- parseFromFile parseInput "input7.txt"
                  Right m_ns -> print $ show (minimum .  allPossibleMoveCosts <$> m_ns)
 
 day7part2 :: IO ()
-day7part2 = pure ()
+day7part2 =  do x <- parseFromFile parseInput "input7.txt"
+    -- do case parse parseInput "" example of
+                case x of
+                 Left pe -> error $ show pe
+                 Right m_ns -> print $ show (minimum .  allPossibleMoveCostsPart2 <$> m_ns)
 
 parseInput :: (Monad m) => ParsecT String u m (Maybe [Int])
 parseInput = mapM readMaybe <$> many1 digit `sepBy` char ','
@@ -22,12 +26,17 @@ example = "16,1,2,0,4,2,7,1,2,14"
 
 -- range :: Ord a => [a] -> [a]
 allPossibleMoveCosts :: (Num a, Ord a, Enum a) => [a] -> [a]
-allPossibleMoveCosts as = map (moves as) (range as)
+allPossibleMoveCosts as = map (fuelToMoveToTarget as) (range as)
+
+allPossibleMoveCostsPart2 :: (Num a, Ord a, Enum a) => [a] -> [a]
+allPossibleMoveCostsPart2 as = map (fuelToMoveToTargetNonLinear as) (range as)
 
 
 range :: (Foldable t, Ord a, Enum a) => t a -> [a]
 range as = [(minimum as) .. (maximum as)]
 
+fuelToMoveToTarget :: Num a => [a] -> a -> a
+fuelToMoveToTarget as target = sum [abs (target - a) | a <- as]
 
-moves :: Num a => [a] -> a -> a
-moves as target = sum [abs (target - a) | a <- as]
+fuelToMoveToTargetNonLinear :: (Num a, Ord a, Enum a) => [a] -> a -> a
+fuelToMoveToTargetNonLinear as target = sum [sum (range [0, abs (target - a)]) | a <- as]
