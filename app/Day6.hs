62027aea254c0ddd01bfc8f747f434a85d305b92
Daniel Barrett <barrett.t.daniel@gmail.com>
2021-12-08 20:16:35 +1100

    day 6 complete

--- app/Day6.hs
+++ app/Day6.hs
@@ -5,26 +5,42 @@ import Text.Parsec
 import Text.Read
 import Debug.Trace
 import Text.Parsec.String (parseFromFile)
+import qualified Data.Map as Map
+import Data.List
 
 day6part1 :: IO ()
-day6part1 = do  
+day6part1 = do
     x <- parseFromFile readInput "input6.txt"
     case x of
         Left pe -> error $ show pe
-        Right m_ns -> trace (show m_ns) $ print (length . simulate <$> m_ns)
+        Right m_ns -> print (sum . map snd . Map.toList . simulate 80 . inputToMap <$> m_ns)
 
 day6part2 :: IO ()
-day6part2 = pure ()
+day6part2 = do
+    x <- parseFromFile readInput "input6.txt"
+    case x of
+        Left pe -> error $ show pe
+        Right m_ns -> print (sum . map snd . Map.toList . simulate 256 . inputToMap  <$> m_ns)
 
 example :: String
 example = "3,4,3,1,2"
 
-day :: Int -> [Int]
-day 0 = [6, 8]
-day n = [n - 1]
+day :: Map.Map Int Int -> Map.Map Int Int
+day = Map.foldrWithKey update (Map.fromList (zip [0..8] (repeat 0)))
 
-simulate :: [Int] -> [Int]
-simulate n = trace (show n) $ iterate (concatMap day) n !! 80
+update :: Int -> Int -> Map.Map Int Int -> Map.Map Int Int
+update 0 value m = Map.update (\z -> (+z) <$> Just value) 6 (Map.update (\z -> (+z) <$> Just value) 8 m)
+update k value m = Map.update (\z -> (+z) <$> Just value) (k - 1) m
+
+simulate :: Int -> Map.Map Int Int -> Map.Map Int Int
+simulate a n = iterate day n !! a
+
+inputToMap :: [Int] -> Map.Map Int Int
+inputToMap as = Map.fromList (countCopies as)
+
+countCopies :: Eq a => [a] -> [(a, Int)]
+countCopies xs = map (\a -> (a, length (filter (==a) xs)))  uniq
+    where uniq = nub xs
 
 readInput :: (Monad m) => ParsecT String u m (Maybe [Int])
 readInput = mapM readMaybe <$> many1 digit `sepBy` char ','
