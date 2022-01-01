75a2b0e1e7883fa3383e232d7b72151f3283196a
Daniel Barrett <barrett.t.daniel@gmail.com>
2022-01-01 21:57:14 +1100

    day 9

--- app/Day9.hs
+++ app/Day9.hs
@@ -1,13 +1,27 @@
+
 module Day9 (day9part1, day9part2) where
 
 import qualified Data.Matrix as M
 import Text.Parsec
-import qualified GHC.OverloadedLabels as M
+import qualified Data.Maybe
+import Control.Monad (filterM, join)
+import Debug.Trace
+import Text.Parsec.String (parseFromFile)
+import Data.List (sort, nub)
 
 day9part1 :: IO ()
-day9part1 = do print $ run 1 1 <$>  M.fromLists <$>  parse parseInput "" example
+day9part1 = do x <- parseFromFile parseInput "input9.txt"
+               case M.fromLists <$> x of
+                 Left pe -> error $ show pe
+                 Right ma -> print $ sum <$> map (+1) <$>  getLowPoints ma
+
 day9part2 :: IO ()
-day9part2 = pure ()
+day9part2 = do x <- parseFromFile parseInput "input9.txt"
+               case M.fromLists <$> x of
+-- day9part2 = do case M.fromLists <$> parse parseInput "" example of
+                 Left pe -> error $ show pe
+                 Right ma -> print $ product <$> take 3 <$> reverse <$> sort <$> map length <$> map nub <$> map (\(x,y,z) -> findBasin x y ma) <$> getLowPointsIndex ma
+
 
 manyDigit :: Monad m =>  ParsecT String u m [Int]
 manyDigit = many1 (read . pure <$> digit)
@@ -15,9 +29,46 @@ manyDigit = many1 (read . pure <$> digit)
 parseInput :: Monad m => ParsecT String u m [[Int]]
 parseInput = manyDigit `sepBy` newline
 
-run :: (Num a, Ord a) => Int -> Int -> M.Matrix a -> Maybe a
-run a b m = minimum <$> sequence [M.safeGet a b m, M.safeGet ((a + 1) `mod` M.nrows m) b m, M.safeGet a ((b + 1) `mod` M.ncols m) m, M.safeGet (a+1) ((b + 1) `mod` M.ncols m) m]
+checkIsPosMin :: (Ord a) => Int -> Int -> M.Matrix a -> Maybe a
+checkIsPosMin a b m = do m <- min
+                         if elem < m then return elem else Nothing
+    where   min = minNeighbour a b m
+            elem = M.getElem  a b m
 
+getLowPoints :: M.Matrix Int -> Maybe [Int]
+getLowPoints ma = sequence $ filter Data.Maybe.isJust $ [checkIsPosMin x y ma | x <- [1..(M.nrows ma)], y <- [1..(M.ncols ma)]]
+
+getLowPointsIndex :: (Ord a) => M.Matrix a -> Maybe [(Int, Int, a)]
+getLowPointsIndex ma = sequence $ filter Data.Maybe.isJust $ [(\s -> (x, y, s)) <$> checkIsPosMin x y ma | x <- [1..(M.nrows ma)], y <- [1..(M.ncols ma)]]
+
+minNeighbour :: (Ord a) => Int -> Int -> M.Matrix a -> Maybe a
+minNeighbour a b m = minimum <$> sequence (filter Data.Maybe.isJust (neighbours a b m))
+
+neighbours :: Int -> Int -> M.Matrix a -> [Maybe a]
+neighbours a b m =[M.safeGet rowsPlusOne b m, M.safeGet a colsPlusOne m, M.safeGet rowsMinusOne b m, M.safeGet a colsMinusOne m]
+    where
+            rowsPlusOne = a + 1
+            rowsMinusOne = a - 1
+            colsPlusOne = b + 1
+            colsMinusOne = b - 1
+
+neighboursIndex :: Int -> Int -> M.Matrix a -> [Maybe (Int, Int, a)]
+neighboursIndex a b m =[safeGetWithIndex rowsPlusOne b m, safeGetWithIndex a colsPlusOne m, safeGetWithIndex rowsMinusOne b m, safeGetWithIndex a colsMinusOne m]
+    where
+            rowsPlusOne = a + 1
+            rowsMinusOne = a - 1
+            colsPlusOne = b + 1
+            colsMinusOne = b - 1
+
+safeGetWithIndex :: Int -> Int -> M.Matrix a -> Maybe (Int, Int, a)
+safeGetWithIndex a b m = case M.safeGet a b m of
+  Nothing -> Nothing
+  Just a' -> Just (a, b, a')
+
+findBasin :: (Ord a, Num a, Show a) => Int -> Int -> M.Matrix a -> [(Int, Int)]
+findBasin a b m =(a,b) : concatMap (\(x,y,z) -> findBasin x y m) (filter (\(x,y,z) -> z == elem + 1 && z /= 9) ns)
+    where   ns = Data.Maybe.fromMaybe [] $ sequence $ filter Data.Maybe.isJust $ neighboursIndex a b m
+            elem = M.getElem  a b m
 
 example :: [Char]
 example = "2199943210\n\
