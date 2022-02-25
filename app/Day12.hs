c604c37ff20bdd7736c63718cdc57394081ac19b
Daniel Barrett <barrett.t.daniel@gmail.com>
2022-02-25 23:02:04 +1100

    start 12

--- app/Day12.hs
+++ app/Day12.hs
@@ -1,26 +1,31 @@
 module Day12  (day12part1, day12part2) where
 import qualified Control.Monad as Monad
-import Text.Parsec (Parsec, many1, (<|>), sepEndBy)
+import Text.Parsec (Parsec, many1, (<|>), sepEndBy, ParsecT, runPT)
 import Text.Parsec.Char (letter)
-import Text.Parsec (char, newline)
+import Text.Parsec (char, newline, parse, runP, runParserT)
 import Text.Parsec.Combinator (sepBy)
-import Text.Parsec.Prim (parse)
+import qualified Control.Monad.Identity as Monad
+import Data.Graph (graphFromEdges)
 
 day12part1 :: IO ()
-day12part1 = pure (parse parseInput "" example) >>= print 
+day12part1 = (\a -> (\(a,_,_) -> a) <$> graphFromEdges  <$>  map toEdge <$> a) <$> runParserT parseInput Monad.Identity "" example >>= print
 
 day12part2 :: Monad.Monad m => m ()
 day12part2 = return ()
 
-parseInput :: Parsec String u [(String, String)]
+parseInput :: ParsecT String u IO [(String, String)]
 parseInput = parseLine `sepEndBy` newline 
 
-parseLine :: Parsec String u (String, String)
+parseLine :: ParsecT String u IO (String, String)
 parseLine = do 
     word1 <- many1 letter
     char '-' 
     word2 <- many1 letter
     return (word1, word2)
+
+toEdge :: (String, String) -> (String, String, [String])
+toEdge (s, a) = (s, s, [a])
+
 
 
 example = "start-A\n\
