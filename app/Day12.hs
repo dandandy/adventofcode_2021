e6146c6aa5893438c52ec58c0a116958c7d317d4
Daniel Barrett <barrett.t.daniel@gmail.com>
2022-03-01 21:43:53 +1100

    use state monad

--- app/Day12.hs
+++ app/Day12.hs
@@ -6,28 +6,44 @@ import Text.Parsec (char, newline, parse, runP, runParserT)
 import Text.Parsec.Combinator (sepBy)
 import qualified Control.Monad.Identity as Monad
 import Data.Graph (graphFromEdges)
+import Data.Map (Map, empty, toList, insertWith)
+import Control.Monad.State (MonadState(state), State, runState, sequence, execState)
+import Day8 (SegDisplay(a))
 
 day12part1 :: IO ()
-day12part1 = (\a -> (\(a,_,_) -> a) <$> graphFromEdges  <$>  map toEdge <$> a) <$> runParserT parseInput Monad.Identity "" example >>= print
+day12part1 = runParserT parseInput Monad.Identity "" example >>= print . ( fst3 <$>  graphFromEdges <$> map toEdge <$> graphFromEdgesState <$>)
 
 day12part2 :: Monad.Monad m => m ()
 day12part2 = return ()
 
 parseInput :: ParsecT String u IO [(String, String)]
-parseInput = parseLine `sepEndBy` newline 
+parseInput = parseLine `sepEndBy` newline
 
 parseLine :: ParsecT String u IO (String, String)
-parseLine = do 
+parseLine = do
     word1 <- many1 letter
-    char '-' 
+    char '-'
     word2 <- many1 letter
     return (word1, word2)
 
-toEdge :: (String, String) -> (String, String, [String])
-toEdge (s, a) = (s, s, [a])
+toEdge :: (String, [String]) -> (String, String, [String])
+toEdge (s, a) = (s, s, a)
+
+graphFromEdgesState :: [(String, String)] -> [(String, [String])]
+graphFromEdgesState as = toList $ execState (mapM toEdgeMapState as) empty
+
+toEdgeMapState :: (String, String) -> State (Map String [String]) ()
+toEdgeMapState sa = state (toEdgeMap sa)
+
+toEdgeMap :: (String, String) -> Map String [String] -> ((), Map String [String])
+toEdgeMap (s, a) m = ((), insertWith (<>) s [a] m)
+
+prependString :: String -> [String] -> Maybe [String]
+prependString a = Just . (a:)
 
 
-
+fst3 :: (a, b, c) -> a
+fst3 (a,b,_) = a
 example = "start-A\n\
 \start-b\n\
 \A-c\n\
